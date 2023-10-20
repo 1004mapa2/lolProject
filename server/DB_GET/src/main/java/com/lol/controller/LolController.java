78388788572d6f8e10b5ff2dc216865e12395091
phj <1004mapa2@naver.com>
2023-10-20 21:41:13 +0900

    feat(BE): 조합 픽률, 승률 반환 로직 구현

--- server/DB_GET/src/main/java/com/lol/controller/LolController.java
+++ server/DB_GET/src/main/java/com/lol/controller/LolController.java
@@ -1,9 +1,22 @@
 package com.lol.controller;
 
+import com.lol.dto.CountDto;
+import com.lol.service.LolService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
+@RequiredArgsConstructor
 public class LolController {
 
-
+    private final LolService lolService;
+    @GetMapping("/1")
+    public Optional<CountDto> test(){
+        Optional<CountDto> count = lolService.getCount();
+        System.out.println(count);
+        return count;
+    }
 }
--- server/DB_GET/src/main/java/com/lol/service/LolService.java
+++ server/DB_GET/src/main/java/com/lol/service/LolService.java
@@ -1,7 +1,44 @@
 package com.lol.service;
 
+import com.lol.dto.ChampionDto;
+import com.lol.dto.ComsaveIdDto;
+import com.lol.dto.CountDto;
+import com.lol.repository.LolMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class LolService {
+
+    private final LolMapper mapper;
+
+    public Optional<CountDto> getCount(){
+
+        ChampionDto championDto = new ChampionDto();
+        championDto.setTopId("43");
+        championDto.setJungleId("64");
+        championDto.setMiddleId("517");
+        championDto.setBottomId("145");
+        championDto.setUtilityId("432");
+        Optional<ChampionDto> comsaveInfo = mapper.getComsaveId(championDto);
+        if(comsaveInfo.isPresent()){
+            ComsaveIdDto comsaveIdDto = new ComsaveIdDto();
+            int comsaveId = comsaveInfo.get().getId();
+            comsaveIdDto.setId(comsaveId);
+
+            int pickCount = mapper.getPickCount(comsaveIdDto);
+            int winCount = mapper.getWinCount(comsaveIdDto);
+            double winRate =(double) winCount / pickCount;
+
+            CountDto countDto = new CountDto();
+            countDto.setPickCount(pickCount);
+            countDto.setWinRate(winRate);
+            return Optional.of(countDto);
+        } else {
+            return null;
+        }
+    }
 }
