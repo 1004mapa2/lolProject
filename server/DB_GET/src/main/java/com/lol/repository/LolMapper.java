84c13de1fd6efd049bd0fd72a5fc90eb2abc5609
phj <1004mapa2@naver.com>
2023-10-29 05:46:33 +0900

    feat(BE): 챔피언 검색 구현

--- server/DB_GET/src/main/java/com/lol/repository/LolMapper.java
+++ server/DB_GET/src/main/java/com/lol/repository/LolMapper.java
@@ -1,6 +1,8 @@
 package com.lol.repository;
 
 import com.lol.dto.AllTierDto;
+import com.lol.dto.ChampionNameDto;
+import com.lol.dto.FindChampionNameDto;
 import com.lol.dto.ReceiveDto;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -11,4 +13,7 @@ public interface LolMapper {
 
     public List<AllTierDto> getAllTier(ReceiveDto receiveDto);
 
+    public List<ChampionNameDto> getAllChampionName(String sort);
+
+    public List<ChampionNameDto> getChampionName(FindChampionNameDto findChampionNameDto);
 }
--- server/DB_GET/src/main/java/com/lol/service/LolService.java
+++ server/DB_GET/src/main/java/com/lol/service/LolService.java
@@ -1,6 +1,8 @@
 package com.lol.service;
 
 import com.lol.dto.AllTierDto;
+import com.lol.dto.ChampionNameDto;
+import com.lol.dto.FindChampionNameDto;
 import com.lol.dto.ReceiveDto;
 import com.lol.repository.LolMapper;
 import lombok.RequiredArgsConstructor;
@@ -14,9 +16,22 @@ public class LolService {
 
     private final LolMapper mapper;
 
-    public List<AllTierDto> getAllTierInfo(ReceiveDto receiveDto){
+    public List<AllTierDto> getAllTierInfo(ReceiveDto receiveDto) {
         List<AllTierDto> allTier = mapper.getAllTier(receiveDto);
 
         return allTier;
+    }
+
+    public List<ChampionNameDto> getChampionNameInfo(String data) {
+        String sort = "CHAMPIONKORNAME";
+        if (data.isEmpty()) {
+            return mapper.getAllChampionName(sort);
+        } else {
+            FindChampionNameDto findChampionNameDto = new FindChampionNameDto();
+            findChampionNameDto.setSort(sort);
+            findChampionNameDto.setInput(data);
+            return mapper.getChampionName(findChampionNameDto);
+        }
+
     }
 }
