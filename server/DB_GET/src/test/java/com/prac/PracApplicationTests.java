932fc45beff40e79e902e3e33dd2e85f2a60241d
phj <1004mapa2@naver.com>
2023-12-12 01:17:40 +0900

    setting(BE): build 파일 업데이트

--- server/DB_GET/src/test/java/com/prac/PracApplicationTests.java
+++ server/DB_GET/src/test/java/com/prac/PracApplicationTests.java
@@ -1,13 +1,8 @@
 package com.prac;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PracApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
