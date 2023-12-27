da7c79c5825580e87821fc7568839865fb99cab3
phj <1004mapa2@naver.com>
2023-12-28 04:41:41 +0900

    feat(BE): swagger jwt 세부 설정

--- server/DB_GET/src/main/java/com/lol/domain/UserAccount.java
+++ server/DB_GET/src/main/java/com/lol/domain/UserAccount.java
@@ -1,5 +1,6 @@
 package com.lol.domain;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.ArrayList;
@@ -9,11 +10,19 @@ import java.util.List;
 @Data
 public class UserAccount {
 
+    @ApiModelProperty(example = "1")
     private int id;
+
+    @ApiModelProperty(example = "유저 아이디")
     private String username;
+
+    @ApiModelProperty(example = "유저 비밀번호")
     private String password;
+
+    @ApiModelProperty(example = "유저 권한")
     private String role;
 
+    @ApiModelProperty(example = "유저 권한 목록")
     public List<String> getRoleList() {
         if(this.role.length() > 0) {
             return Arrays.asList(this.role.split(","));
--- server/DB_GET/src/main/java/com/lol/dto/user/UserUpdateDto.java
+++ server/DB_GET/src/main/java/com/lol/dto/user/UserUpdateDto.java
@@ -1,10 +1,14 @@
 package com.lol.dto.user;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class UserUpdateDto {
 
+    @ApiModelProperty(example = "기존 비밀번호")
     private String originalPassword;
+
+    @ApiModelProperty(example = "새 비밀번호")
     private String newPassword;
 }
